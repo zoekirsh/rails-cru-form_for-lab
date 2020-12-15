f38b868899fc939aa49a7919e203965cd15ae7d7
Zoe Kirsh <zoekirsh@gmail.com>
2020-12-15 11:32:58 -0800

    all passing

---
+++ app/helpers/arists_helper.rb
@@ -0,0 +1,2 @@
+module AristsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :genres, only: [:new, :create, :show, :edit, :update]
+  resources :artists, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
